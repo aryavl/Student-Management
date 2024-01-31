32a47ae06feb46b1ff06831356cadc0ec0136b34
arya <aryavl9813@gmail.com>
2024-01-31 23:38:51 +0530

    Changed types in auth

--- src/app/api/(next-auth)/auth/[...nextauth]/route.ts
+++ src/app/api/(next-auth)/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 // "use client"
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialProvider from "next-auth/providers/credentials";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import CredentialProvider, { CredentialsConfig } from "next-auth/providers/credentials";
 import connect from "@/utils/db";
 import Admin from "@/models/Admin";
 import bcrypt from "bcryptjs";
@@ -14,18 +14,33 @@ import {
 import { NextApiRequest, NextApiResponse } from "next";
 import Teachers from "@/models/Teachers";
 import Student from "@/models/Student";
+import { OAuthConfig } from "next-auth/providers/oauth";
+import { Awaitable, RequestInternal } from "next-auth";
 // const dispatch = useDispatch()
 interface CredentialType{
   email: string;
   password: string;
   role: "admin" | "teacher" | "student"
 }
-export const authOptions = {
+export const authOptions:{
+  providers: (OAuthConfig<GoogleProfile> | CredentialsConfig<{}>)[];
+  pages: {
+      signIn: string;
+      signInteacher: string;
+      oauth: string;
+  };
+  callbacks: {
+    signIn({ account, profile, email, credentials }: any): Promise<true | st>;
+    jwt({ token, user, trigger, session }: any): Promise<any>;
+    session({ session, token }: any): Promise<any>;
+};
+//  authorize: (credentials: Record<never, string> | undefined, req: Pick<RequestInternal, "body" | "method" | "query" | "headers">) => Awaitable<UserType|null|any>
+  
+} = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      
     }),
     CredentialProvider({
       name: "credentials",
@@ -36,81 +51,81 @@ export const authOptions = {
         const { email, password, role } = credentials as {
           email: string;
           password: string;
-          role: "admin" | "teacher" | "student"
+          role: "admin" | "teacher" | "student";
         };
-        if(!role || (role !== 'admin' && role !== 'teacher' && role !== 'student'))
-           throw new Error("Invalid Role.");
+        if (!role || (role !== 'admin' && role !== 'teacher' && role !== 'student'))
+          throw new Error("Invalid Role.");
         try {
           await connect();
-          if(role === "admin"){
-            const admin: AdminDocument | null = await Admin.findOne({ email });  
+          if (role === "admin") {
+            const admin: AdminDocument | null = await Admin.findOne({ email });
             if (!admin) {
               throw new Error("Incorrect email");
             }
-  
+
             const passwordMatch = await bcrypt.compare(password, admin.password);
-  
+
             if (!passwordMatch) {
               throw new Error("Incorrect password");
             }
-  
+
             return {
               id: admin._id,
               email: admin.email,
               firstname: admin.firstname,
               lastname: admin.lastname,
               name: `${admin.firstname} ${admin.lastname}`,
-              role:'admin'
+              role: 'admin'
             };
           }
-          if(role === 'teacher'){
+          if (role === 'teacher') {
             console.log("inside teacher");
-            
-            const teacher = await Teachers.findOne({ email }); 
+
+            const teacher = await Teachers.findOne({ email });
             console.log(teacher._id.toString());
-             
+
             if (!teacher) {
               throw new Error("Incorrect email");
             }
-  
+
             const passwordMatch = await bcrypt.compare(password, teacher.password);
-  
+
             if (!passwordMatch) {
               throw new Error("Incorrect password");
             }
-  
+
             return {
               id: teacher._id,
               email: teacher.email,
               teacherName: teacher.teacherName,
-              isVerified:teacher.isVerified,
-              isClassTeacher:teacher.isClassTeacher,
-              role:"teacher"
-            }
+              isVerified: teacher.isVerified,
+              isClassTeacher: teacher.isClassTeacher,
+              role: "teacher"
+            };
           }
-          if(role === 'student'){
+          if (role === 'student') {
             console.log("inside student");
-            
-            const student = await Student.findOne({ email }); 
+
+            const student = await Student.findOne({ email });
             console.log(student._id.toString());
-             
+
             if (!student) {
               throw new Error("Incorrect email");
             }
-  
+
             const passwordMatch = await bcrypt.compare(password, student.password);
-  
+
             if (!passwordMatch) {
               throw new Error("Incorrect password");
             }
-  
+
             return {
               id: student._id,
               email: student.email,
               studentName: student.studentName,
-              isVerified:student.isVerified,
-              role:"teacher"
-            }
+              isVerified: student.isVerified,
+              role: "teacher"
+            };
           }
         } catch (error: any) {
           console.error("Error in credential auth ==>", error.message);
@@ -136,27 +151,29 @@ export const authOptions = {
 
       return true;
     },
-    async jwt({ token,user: USER, trigger, session }: any) {
+    async jwt({ token, user: USER, trigger, session }: any) {
       console.log({ token }, session, USER);
-      
-      console.log({token},session);
-      const user = await getUserByEmail({email:token.email})
+
+      console.log({ token }, session);
+      const user = await getUserByEmail({ email: token.email });
       // console.log({user});
-  
-      token.user = user
+      token.user = user;
       token.user = {
         ...user,
         type: user.type
       };
-    return token;
+      return token;
     },
     async session({ session, token }: any) {
-      console.log(session,token);
-            session.user = token.user
-            return session;
-  
+      console.log(session, token);
+      session.user = token.user;
+      return session;
+
     },
   },
+  // authorize: function (credentials: Record<never, string> | undefined, req: Pick<RequestInternal, "body" | "method" | "query" | "headers">) {
+  //   throw new Error("Function not implemented.");
+  // }
 };
 
 
